4ca0a6f5f9fd5da8e735e11302d7f3dffaf0d515
Thorsten <info@itviking.net>
2024-07-05 06:09:25 -0700

    feat: requirements 1,2,3 implementation

--- lib/vandelay/rest/patients_patient.rb
+++ lib/vandelay/rest/patients_patient.rb
@@ -5,7 +5,23 @@ module Vandelay
   module REST
     module PatientsPatient
       def self.registered(app)
-        # add endpoint code here
+        app.get '/patients/patient/:patient_id' do
+          begin
+            json(Vandelay::Services::Patients.new.retrieve_one(params[:patient_id]))
+          rescue
+            halt 404, json({ error: 'Record not found' })
+          end
+        end
+
+        app.get '/patients/:patient_id/record' do
+          begin
+            result = {}
+            results=Vandelay::Services::PatientRecords.new.retrieve_record_for_patient(params[:patient_id])
+            JSON(results)
+          rescue
+            halt 404, json({ error: 'Record not found' })
+          end
+        end
       end
     end
   end
--- lib/vandelay/util/cache.rb
+++ lib/vandelay/util/cache.rb
@@ -1,9 +1,25 @@
 require 'redis'
+require 'json'
 
 module Vandelay
   module Util
     class Cache
-      # your code here
+      def initialize(redis_host: 'redis', redis_port: 6379)
+        @redis = Redis.new(host: redis_host, port: redis_port)
+      end
+
+      def fetch_and_cache(key, expiration = 600, &block)
+        cache_key = "cache:#{key}"
+        cached_result = @redis.get(cache_key)
+
+        if cached_result
+          JSON.parse(cached_result)
+        else
+          result = yield
+          @redis.set(cache_key, result.to_json, ex: expiration)
+          result
+        end
+      end
     end
   end
 end
